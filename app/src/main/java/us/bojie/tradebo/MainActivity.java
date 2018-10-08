208976d57be40e02730c981ece05dfa80d055d17
Bojie Jiang <jiangbojie@gmail.com>
2018-10-07 23:17:51 -0700

    Setup

---
+++ app/src/main/java/us/bojie/tradebo/MainActivity.java
@@ -0,0 +1,20 @@
+package us.bojie.tradebo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import us.bojie.tradebo.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ app/src/main/java/us/bojie/tradebo/ui/main/MainViewModel.java
@@ -0,0 +1,8 @@
+package us.bojie.tradebo.ui.main;
+
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
